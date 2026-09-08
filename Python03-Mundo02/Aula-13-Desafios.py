4cf2e8acc3b366c6a549c8ae611b7f4874e2dce3
Livia Rocha <liviarocha640@gmail.com>
2026-09-08 10:38:28 -0300

    desafio-054

--- Python03-Mundo02/Aula-13-Desafios.py
+++ Python03-Mundo02/Aula-13-Desafios.py
@@ -136,3 +136,28 @@ if inverso == junto:
     print("Temos um palíndromo!")
 else:
     print("A frase digitada não é um palíndromo!")
+
+
+#%%
+
+#Exercício Python 54: Crie um programa que leia o ano de nascimento de sete pessoas. 
+#No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+
+print("DESAFIO 054")
+
+from datetime import date
+
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+
+for pess in range(1, 8):
+    nasc = int(input(f"Em que ano a {pess} pessoa nasceu? "))
+    idade = atual - nasc 
+    if idade >= 21:
+        totmaior += 1
+    else:
+        totmenor += 1
+
+print(f"Ao todo tivemos {totmaior} pessoas maiores de idade")
+print(f"E também tivemos {totmenor} pessoas menores de idade")
