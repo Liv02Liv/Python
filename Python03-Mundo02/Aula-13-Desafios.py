22dadc41c165ad8bdb381aa9e2faf2aa74c57c2c
Livia Rocha <liviarocha640@gmail.com>
2026-09-08 10:24:55 -0300

    desafio-053

--- Python03-Mundo02/Aula-13-Desafios.py
+++ Python03-Mundo02/Aula-13-Desafios.py
@@ -115,3 +115,24 @@ if tot == 2:
     print("E por isso ele é primo!")
 else:
     print("E por isso ele Não é primo!")
+
+#%%
+
+#Exercício Python 53: Crie um programa que leia uma frase qualquer 
+#e diga se ela é um palíndromo, desconsiderando os espaços. Exemplos de palíndromos:
+
+#APÓS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.
+
+print("DESAFIO 053")
+
+frase = str(input("Digite uma frase: ")).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+
+print(f"O inverso de {junto} é {inverso}")
+
+if inverso == junto:
+    print("Temos um palíndromo!")
+else:
+    print("A frase digitada não é um palíndromo!")
