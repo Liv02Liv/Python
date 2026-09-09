8b0051540d9a8dc386140db95993fa17b28cc0a0
Livia Rocha <liviarocha640@gmail.com>
2026-09-09 09:44:02 -0300

    desafio-055

--- Python03-Mundo02/Aula-13-Desafios.py
+++ Python03-Mundo02/Aula-13-Desafios.py
@@ -161,3 +161,27 @@ for pess in range(1, 8):
 
 print(f"Ao todo tivemos {totmaior} pessoas maiores de idade")
 print(f"E também tivemos {totmenor} pessoas menores de idade")
+
+
+#%%
+
+
+#Exercício Python 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+maior = 0
+menor = 0
+
+for p in range(1, 6):
+    peso = float(input(f"Peso da {p} pessoa: "))
+
+    if p == 1:
+        maior = peso
+        menor = peso 
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+
+print(f"O maior peso lido foi de {maior}Kg")
+print(f"O menor peso lido foi de {menor}Kg")
