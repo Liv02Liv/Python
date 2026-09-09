d795253384d097b3399c801f4189ce34ffdcff47
Livia Rocha <liviarocha640@gmail.com>
2026-09-09 10:07:05 -0300

    desafio-056

--- Python03-Mundo02/Aula-13-Desafios.py
+++ Python03-Mundo02/Aula-13-Desafios.py
@@ -168,6 +168,8 @@ print(f"E também tivemos {totmenor} pessoas menores de idade")
 
 #Exercício Python 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
+print("DESAFIO 055")
+
 maior = 0
 menor = 0
 
@@ -185,3 +187,42 @@ for p in range(1, 6):
 
 print(f"O maior peso lido foi de {maior}Kg")
 print(f"O menor peso lido foi de {menor}Kg")
+
+
+#%%
+
+#Exercício Python 56: Desenvolva um programa que leia o nome, 
+#idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, 
+#qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+
+print("DESAFIO 056")
+
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = 0
+totmulher20 = 0
+
+for p in range(1, 5):
+    print(f"----- {p} PESSOA -----")
+    nome = str(input("Nome: ")).strip()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo [M/F]: ")).strip()
+    somaidade += idade
+
+    if p == 1 and sexo in "Mm":
+        maioridadehomem = idade
+        nomevelho = nome
+
+    if sexo in "Mm" and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+
+    if sexo in "Ff" and idade < 20:
+        totmulher20 += 1
+
+mediaidade = somaidade / 4
+
+print(f"A  média de idade do grupo é de {mediaidade} anos")
+print(f"O homem mais velho tem {maioridadehomem} anos e se chama {nomevelho}")
+print(f"Ao todo são {totmulher20} mulheres com menos de 20 anos")
