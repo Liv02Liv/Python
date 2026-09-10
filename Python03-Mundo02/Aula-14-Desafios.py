250402b5bd019c9aa9dd89fa9a016f88191f82b8
Livia Rocha <liviarocha640@gmail.com>
2026-09-10 11:46:49 -0300

    2-Parte

--- Python03-Mundo02/Aula-14-Desafios.py
+++ Python03-Mundo02/Aula-14-Desafios.py
@@ -14,3 +14,23 @@ while c < 10:
     print(c)
     c = c + 1
 print("Acabou!")
+
+#%%
+
+n = 1
+
+while n != 0:
+    n = int(input("Digite um valor: "))
+    print(n)
+print("Fim!")
+
+#%%
+
+n = 1
+r = "S"
+
+while r == "S":
+    n = int(input("Digite um valor: "))
+    r = str(input("Quer continuar? [S/N]")).upper()
+    print(n)
+print("Fim!")
